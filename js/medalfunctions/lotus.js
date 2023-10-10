e94371aa9cc0432f2a31347e9ed896ca1eb7e806
MAX POLLOCK <mcpollock00@gmail.com>
2023-10-10 16:24:15 +0100

    Added buttons to log use of applications on page

--- js/medalfunctions/lotus.js
+++ js/medalfunctions/lotus.js
@@ -1,8 +1,5 @@
 let lotusClicks = localStorage.getItem("Lotus Clicks") || 0;
 
-// let lotus = document.getElementById("start");
-// lotus.addEventListener("click", lotusClick);
-
 function lotusClick() {
   lotusClicks++;
   localStorage.setItem("Lotus Clicks", lotusClicks);
