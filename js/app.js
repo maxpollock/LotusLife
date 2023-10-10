3e2aab24d14153c89849a1b040fcf3f398df5f98
kara sealeaf <kara.sealeaf13@gmail.com>
2023-10-10 14:52:09 +0100

    button in js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@ function breathPausePlayToggle() {
 
   circle.style.animationPlayState = running ? "paused" : "running";
 }
+
+const button = document.querySelector(".start");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault;
+  button.classList.add("animate");
+  setTimeout(() => {
+    button.classList.remove("animate");
+  }, 600);
+});
